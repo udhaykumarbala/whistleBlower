c9036069210bcb05ba5948bf0b4635383dd77957
udhaykumarbala <udhaykumar97@outlook.com>
2018-02-07 18:32:58 +0530

    index1

---
+++ index1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>index</title>
+</head>
+<body>
+
+<div >
+  <form id="company_name">
+    <label>type the company name
+    <br>
+  	<input type="text" name="company_search"></label>
+  </form>
+  <form id="blown_whistle">
+    <input type="text" name="id" placeholder="XXXX-XXXX-XXXX-XXXX">
+  </form>
+</div>
+
+<div id="modal">
+   foreach
+
+</div>
+</body>
+</html>
